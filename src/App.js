95ac35a876ae3420ff4676bc16b09d2ead0737d9
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-04-03 18:12:46 +0500

    Logo  added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
 
 function App() {
   return (

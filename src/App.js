0471585dec215c7141fefa165862134063db1924
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-04-03 16:07:58 +0500

    Navigation.js added and simple signout written

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+      {/*
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition />
+       */}
+    </div>
+  );
 }
 
 export default App;

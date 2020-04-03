0b6cfe256ff4fd6d9ec1554063531533b29a5a21
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-04-03 18:51:51 +0500

    video2 till 30:15

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <Navigation />
-      <Rank />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*
       <FaceRecognition />
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Rank = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="f3 white">{"Sajeel, your current rank is...."}</div>
+      <div className="f1 white">{"#5"}</div>
+    </div>
+  );
 };
 
 export default Rank;

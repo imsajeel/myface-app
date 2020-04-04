fd691247f181ae73d4ad8f5e954096d3c2e29f67
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-04-04 11:41:49 +0500

    onInputChange and state added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Particles from "react-particles-js";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
@@ -18,19 +18,32 @@ const particlesOptions = {
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ""
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} />
+        {/*
       <FaceRecognition />
        */}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;

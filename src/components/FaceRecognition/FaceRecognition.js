df627d1288eb8f6d49a9e4e6ae09529e5b069f30
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-04-05 10:30:39 +0500

    video#6 completed

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,6 +1,7 @@
 import React from "react";
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className="ma">
       <div className="absolute center mt2">
@@ -11,7 +12,15 @@ const FaceRecognition = ({ imageUrl }) => {
           width="500px"
           height="auto"
         />
-        <div className="bounding-box"></div>
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        ></div>
       </div>
     </div>
   );
